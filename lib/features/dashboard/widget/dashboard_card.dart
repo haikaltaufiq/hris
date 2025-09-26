99e1d0fc7d14af43d1b0dd12d328d73a31fa99fc
M Taufiq Karim Haikal <haikal.taufiiq@gmail.com>
2025-09-26 15:27:38 +0700

    Fix Card

--- lib/features/dashboard/widget/dashboard_card.dart
+++ lib/features/dashboard/widget/dashboard_card.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hr/core/theme/app_colors.dart';
 import 'package:hr/features/auth/login_viewmodels.dart/login_provider.dart';
-import 'package:hr/features/dashboard/widget/hover_tooltip.dart';
 import 'package:hr/features/department/view_model/department_viewmodels.dart';
 import 'package:hr/features/task/task_viewmodel/tugas_provider.dart';
 import 'package:provider/provider.dart';
@@ -83,65 +82,62 @@ class _DashboardCardState extends State<DashboardCard> {
   }
 
   Widget _buildCard(BuildContext context, Map<String, dynamic> data) {
-    return HoverTooltip(
-      message: "${data['title']}: ${data['value']}",
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: AppColors.primary,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.05), // tipis banget
-                  blurRadius: 4, // kecil, biar soft
-                  spreadRadius: 0,
-                  offset: Offset(0, 1), // cuma bawah dikit
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05), // tipis banget
+                blurRadius: 4, // kecil, biar soft
+                spreadRadius: 0,
+                offset: Offset(0, 1), // cuma bawah dikit
+              ),
+            ],
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data['title'],
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    color: AppColors.putih,
+                  ),
+                ),
+                Text(
+                  data['value'],
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: AppColors.putih,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                  ),
                 ),
               ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['title'],
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: GoogleFonts.poppins().fontFamily,
-                      color: AppColors.putih,
-                    ),
-                  ),
-                  Text(
-                    data['value'],
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: AppColors.putih,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: GoogleFonts.poppins().fontFamily,
-                    ),
-                  ),
-                ],
-              ),
+          ),
+        ),
+        Positioned(
+          bottom: -10,
+          right: 10,
+          child: SizedBox(
+            height: max(130, MediaQuery.of(context).size.height * 0.12) * 0.9,
+            child: Image.asset(
+              data['icon'],
+              fit: BoxFit.contain,
             ),
           ),
-          Positioned(
-            bottom: -10,
-            right: 10,
-            child: SizedBox(
-              height: max(130, MediaQuery.of(context).size.height * 0.12) * 0.9,
-              child: Image.asset(
-                data['icon'],
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
