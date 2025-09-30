af11937f3d402814d55667ac7a5aec953bb503af
Greyari <159263690+Greyari@users.noreply.github.com>
2025-09-30 09:45:19 +0700

    fix bug export gaji

--- lib/data/services/helper/gaji_helper_web.dart
+++ lib/data/services/helper/gaji_helper_web.dart
@@ -1,3 +1,4 @@
+// helper/gaji_helper_web.dart
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
@@ -5,6 +6,11 @@ class GajiWebHelper {
   static void downloadFile(List<int> bytes, String fileName) {
     final blob = html.Blob([bytes]);
     final url = html.Url.createObjectUrlFromBlob(blob);
+
+    // ignore: unused_local_variable
+    final anchor = html.AnchorElement(href: url)
+      ..setAttribute("download", fileName)
+      ..click();
 
     html.Url.revokeObjectUrl(url);
   }
